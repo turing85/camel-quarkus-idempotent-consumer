2c0a73dbc8bf17c28de0c3f2f530e7fddd2d0abc
Marco Bungart <marco.bungart@googlemail.com>
2023-02-02 17:05:02 +0100

    implementation for default repository to show that table creation does not work

--- src/main/java/de/turing85/Route.java
+++ src/main/java/de/turing85/Route.java
@@ -2,76 +2,25 @@ package de.turing85;
 
 import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.file;
 
-import de.turing85.config.AdapterConfig;
-import de.turing85.config.IdempotencyConfig;
 import javax.enterprise.context.ApplicationScoped;
 import javax.sql.DataSource;
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.idempotent.jdbc.JdbcMessageIdRepository;
 
 @ApplicationScoped
 public class Route extends RouteBuilder {
   public static final String ROUTE_ID = "file-mover";
-  private final CustomJdbcMessageIdRepository idempotentRepository;
+  private final JdbcMessageIdRepository idempotentRepository;
 
-  public Route(
-      @SuppressWarnings("CdiInjectionPointsInspection") DataSource dataSource,
-      AdapterConfig adapterConfig,
-      CamelContext context) {
+  public Route(@SuppressWarnings("CdiInjectionPointsInspection") DataSource dataSource) {
     idempotentRepository =
-        constructRepository(dataSource, adapterConfig, context);
+        constructRepository(dataSource);
   }
 
-  private static CustomJdbcMessageIdRepository constructRepository(
-      DataSource dataSource,
-      AdapterConfig adapterConfig,
-      CamelContext context) {
-    IdempotencyConfig idempotencyConfig = adapterConfig.idempotencyConfig();
-    String adapterName = adapterConfig.name();
-    final CustomJdbcMessageIdRepository repository =
-        new CustomJdbcMessageIdRepository(dataSource, ROUTE_ID, context);
-    repository.setTableName(idempotencyConfig.tableName());
-    repository.setLockMaxAgeMillis(idempotencyConfig.lockMaxAge().toMillis());
-    repository.setLockKeepAliveIntervalMillis(idempotencyConfig.keepAliveInterval().toMillis());
-    repository.setCreateTableIfNotExists(false);
-    repository.setCreateString("""
-        CREATE TABLE CAMEL_MESSAGEPROCESSED (
-            adapterName VARCHAR(255),
-            processorName VARCHAR(255),
-            messageId VARCHAR(100),
-            createdAt TIMESTAMP,
-            done BOOLEAN DEFAULT false,
-            PRIMARY KEY (adapterName, processorName, messageId)
-        )
-        """);
-    repository.setInsertString("""
-        INSERT INTO CAMEL_MESSAGEPROCESSED (
-            adapterName,
-            processorName,
-            messageId,
-            createdAt)
-        VALUES (
-            '%s',
-            ?,
-            ?,
-            ?)
-        """.formatted(adapterName));
-    repository.setUpdateDoneString("""
-        UPDATE CAMEL_MESSAGEPROCESSED
-        SET done = true, createdAt = ?
-        WHERE
-            adapterName = '%s' AND
-            processorName = ? AND
-            messageId = ?
-        """.formatted(adapterName));
-    repository.setQueryString("""
-        SELECT COUNT(*)
-        FROM CAMEL_MESSAGEPROCESSED
-        WHERE
-            adapterName = '%s' AND
-            processorName = ? AND
-            messageId = ?
-        """.formatted(adapterName));
+  private static JdbcMessageIdRepository constructRepository(DataSource dataSource) {
+    final JdbcMessageIdRepository repository =
+        new JdbcMessageIdRepository(dataSource, ROUTE_ID);
+    repository.setCreateTableIfNotExists(true);
     return repository;
   }
 
